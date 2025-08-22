4d16218491edc9001e05a3adf45d1dcaf92e9c88
Guaru34 <19170095@uttcampus.edu.mx>
2025-08-21 21:33:21 -0600

    añadir funcion para generar calculo de palabra entre un minimo y maximo de letras segun la dificultad

--- consts.ts
+++ consts.ts
@@ -1,13 +1,15 @@
+import { getRandomLength } from "./functions";
+
 export const LANGUAGES = {
     SPANISH: 'es',
     ENGLISH: 'en'
 }
 
 export const DIFFICULTYS = {
-    FACIL: { name: "Fácil", value: 0, wordLength: 5, attempts: 10 },
-    INTERMEDIO: { name: "Intermedio", value: 1, wordLength: 7, attempts: 6 },
-    DIFICIL: { name: "Difícil", value: 2, wordLength: 10, attempts: 4 },
-    IMPOSIBLE: { name: "Imposible", value: 3, wordLength: 15, attempts: 1 },
+    FACIL: { name: "Fácil", value: 0, wordLength: getRandomLength(1, 5), attempts: 10 },
+    INTERMEDIO: { name: "Intermedio", value: 1, wordLength: getRandomLength(6, 7), attempts: 6 },
+    DIFICIL: { name: "Difícil", value: 2, wordLength: getRandomLength(8, 10), attempts: 4 },
+    IMPOSIBLE: { name: "Imposible", value: 3, wordLength: getRandomLength(11, 15), attempts: 1 },
 };
 
 export const DIFFICULTYS_MAP = new Map(
